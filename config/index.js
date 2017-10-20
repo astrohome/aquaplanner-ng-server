304bcfe103c2845d061774df7c7836002a03fb5c
Illia Izotov <izotov.ilya@gmail.com>
2017-10-21 00:08:18 +0200

    Proxying API calls.

--- config/index.js
+++ config/index.js
@@ -31,7 +31,9 @@ module.exports = {
     autoOpenBrowser: false,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api/*': 'http://localhost:8080/'
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
