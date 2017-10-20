d061914d582b8d6ad5c0233bae537f4c1c2a18c4
Illia Izotov <izotov.ilya@gmail.com>
2017-10-21 00:04:59 +0200

    Router History mode.

--- src/main/vuejs/router/index.js
+++ src/main/vuejs/router/index.js
@@ -6,7 +6,7 @@ import Channel from '../components/channels/Channel.vue'
 Vue.use(Router)
 
 export default new Router({
-//    mode: 'history',
+  mode: 'history',
   routes: [
     {
       path: '/led/tasks',
